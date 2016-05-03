ddc6ed680809db35584b8476845d8faf33db73d4
Васильев Сергей <g.o.m.e.z@yandex.ru>
2016-05-03 14:27:09 +0300

    add  Window

---
+++ src/MyWindow.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * Created by Администратор on 03.05.2016.
+ */
+public class MyWindow extends JFrame {
+    public MyWindow(){
+        super("Spline Interpolation");
+        setBounds(100,100,200,200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
