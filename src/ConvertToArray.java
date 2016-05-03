cb6510b317ae111141c102121eb932cecd8238bf
Васильев Сергей <g.o.m.e.z@yandex.ru>
2016-05-03 15:16:48 +0300

    add Creating a class newline

---
+++ src/ConvertToArray.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Администратор on 03.05.2016.
+ */
+public class ConvertToArray {
+    static public double[] OfString(String string){
+        return new double[4];
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
         public static void main(String[] args) {
-                //Spline spline = new Spline();
                 MyWindow window = new MyWindow();
                 window.setVisible(true);
+
         }
 }
